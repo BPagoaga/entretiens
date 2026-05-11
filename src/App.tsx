3edf0a86239b32d60c6d0cb4186b549ce2ffcf65
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2026-05-11 14:23:04 +0200

    add models

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { AppRoutes } from './routes/AppRoutes';
-
+import './configure-amplify';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './react-query.client';
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AppRoutes />
-    </>
+    </QueryClientProvider>
   );
 }
 
